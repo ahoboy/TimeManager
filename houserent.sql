91f40b089699a29b3bbdab3ac34eb1a92433be8e
ahoboy <ahoboy@foxmail.com>
2020-05-08 10:05:21 +0800

    修改房源标识为int类型

--- houserent.sql
+++ houserent.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80020
  File Encoding         : 65001
 
- Date: 08/05/2020 08:01:26
+ Date: 08/05/2020 10:04:28
 */
 
 SET NAMES utf8mb4;
@@ -27,7 +27,7 @@ CREATE TABLE `house`  (
   `Hprice` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `Hadress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `Hsize` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Hflag` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Hflag` int(0) NOT NULL,
   `Hdesc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `Uid` int(0) NULL DEFAULT NULL,
   PRIMARY KEY (`Hid`) USING BTREE,
@@ -37,12 +37,12 @@ CREATE TABLE `house`  (
 -- ----------------------------
 -- Records of house
 -- ----------------------------
-INSERT INTO `house` VALUES (1, '香格丽舍', '666666', '金水市', '500', '0', '无', 1);
-INSERT INTO `house` VALUES (2, '爱琴堡', '999999', '天元镇', '1000', '0', '无', 2);
-INSERT INTO `house` VALUES (3, '忘忧阁', '222222', '飞音镇', '300', '0', '无', 3);
-INSERT INTO `house` VALUES (4, '凌月馆', '222222', '日川市', '200', '0', '无', 4);
-INSERT INTO `house` VALUES (5, '花果园', '555555', '道古市', '600', '0', '无', 5);
-INSERT INTO `house` VALUES (6, '七里香都', '888888', '西达镇', '700', '0', '无', 6);
+INSERT INTO `house` VALUES (1, '香格丽舍', '666666', '金水市', '500', 0, '无', 1);
+INSERT INTO `house` VALUES (2, '爱琴堡', '999999', '天元镇', '1000', 0, '无', 2);
+INSERT INTO `house` VALUES (3, '忘忧阁', '222222', '飞音镇', '300', 0, '无', 3);
+INSERT INTO `house` VALUES (4, '凌月馆', '222222', '日川市', '200', 0, '无', 4);
+INSERT INTO `house` VALUES (5, '花果园', '555555', '道古市', '600', 0, '无', 5);
+INSERT INTO `house` VALUES (6, '七里香都', '888888', '西达镇', '700', 0, '无', 6);
 
 -- ----------------------------
 -- Table structure for owner
@@ -66,5 +66,6 @@ INSERT INTO `owner` VALUES (3, '王五', 'wangwu', '1230222032', '男');
 INSERT INTO `owner` VALUES (4, '麻婆', 'mapo', '1234315354', '女');
 INSERT INTO `owner` VALUES (5, '艾米', 'aimi', '1563156345', '女');
 INSERT INTO `owner` VALUES (6, '琪亚娜', 'qiyana', '4534245646', '女');
+INSERT INTO `owner` VALUES (9, '香菜', 'xiangcai', '12514256589', 'female');
 
 SET FOREIGN_KEY_CHECKS = 1;
